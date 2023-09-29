89011f7404f40ff056fdde1abcbba8d290d024db
victorpereiira <victorrpsantos@gmail.com>
2023-09-29 01:04:44 -0300

    feat: execution

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,8 @@ import YouTube from 'react-youtube';
 import peso from "../../assets/icons/peso.svg"
 import repeats from "../../assets/icons/repeats.svg"
 import series from "../../assets/icons/series.svg"
+import list from "../../assets/icons/list.svg"
+import exec from "../../assets/icons/exec.svg"
 
 // Components
 
@@ -89,6 +91,13 @@ function Home() {
     ]
 
 
+    const [listMode, setListMode] = useState(true)
+    const [IconMode, setIconMode] = useState(exec)
+    function handleMode() {
+        setIconMode(IconMode === list ? exec : list)
+        setListMode(!listMode)
+    }
+
     const [modal, setModal] = useState(false)
     const [videoId, setVideoId] = useState("")
     function handleClick(videoIdFlag: string) {
@@ -97,71 +106,153 @@ function Home() {
     }
 
 
+    const [count, setCount] = useState(0)
+    const [serie, setSerie] = useState(1)
+    const [isStart, setIsStart] = useState(false)
+    const execTitle = data.map(el => {
+        if (el.days === "MT") {
+            return el
+        }
+    })
+    function startExec() {
+        setIsStart(!isStart)
+        handleClick(execTitle[count]?.videoId || "")
+    }
+    function handleExec() {
+        if (serie < 3) {
+            setSerie(serie + 1)
+            handleClick(execTitle[count]?.videoId || "")
+        } else {
+            setCount(count + 1)
+            handleClick(execTitle[count + 1]?.videoId || "")
+            setSerie(1)
+        }
+        if (count === 3 && serie === 3) {
+            setIsStart(false)
+            setCount(0)
+            setSerie(1)
+            setModal(!modal)
+        }
+    }
+
+
     return (
         <Wrapper>
-            <p className="py-[8px] px-[16] text-white text-[20px] bg-orange1 w-[100px] text-center rounded-md mb-[32px]">Week 1</p>
-            <ul className="pb-[20px]">
-                <li className="flex gap-[8px] pb-[16px]">
-                    <p className="bg-orange1 text-white rounded-full w-[24px] h-[24px] text-center">1</p>
-                    <p className="text-[16px] text-orange1">Monday and Thusday</p>
-                </li>
-                {
-                    data.map(el => {
-                        if (el.days === "MT") {
-                            return (
-                                <li className="flex justify-between items-center pb-[10px]" onClick={() => handleClick(el.videoId)}>
-                                    <h2 className="text-white">{el.title}</h2>
-                                    <section className="flex gap-[22px]">
-                                        <div className="flex gap-[8px]">
-                                            <img src={series} />
-                                            <p className="text-white">{el.series}</p>
-                                        </div>
-                                        <div className="flex gap-[8px]">
-                                            <img src={repeats} />
-                                            <p className="text-white">{el.repeats}</p>
-                                        </div>
-                                        <div className="flex gap-[8px]">
-                                            <img src={peso} />
-                                            <p className="text-white">{el.peso}</p>
-                                        </div>
-                                    </section>
-                                </li>
-                            )
+            <section className="flex justify-between items-center mb-[32px]">
+                <p className="py-[8px] px-[16] text-white text-[20px] bg-orange1 w-[100px] text-center rounded-md">Week 1</p>
+                <img src={IconMode} onClick={handleMode} />
+            </section>
+            {
+                listMode ?
+                    <section>
+                        <ul className="pb-[20px]">
+                            <li className="flex gap-[8px] pb-[16px]">
+                                <p className="bg-orange1 text-white rounded-full w-[24px] h-[24px] text-center">1</p>
+                                <p className="text-[16px] text-orange1">Monday and Thusday</p>
+                            </li>
+                            {
+                                data.map(el => {
+                                    if (el.days === "MT") {
+                                        return (
+                                            <li className="flex justify-between items-center pb-[10px]" onClick={() => handleClick(el.videoId)}>
+                                                <h2 className="text-white">{el.title}</h2>
+                                                <section className="flex gap-[22px]">
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={series} />
+                                                        <p className="text-white">{el.series}</p>
+                                                    </div>
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={repeats} />
+                                                        <p className="text-white">{el.repeats}</p>
+                                                    </div>
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={peso} />
+                                                        <p className="text-white">{el.peso}</p>
+                                                    </div>
+                                                </section>
+                                            </li>
+                                        )
+                                    }
+                                })
+                            }
+                        </ul>
+                        <ul className="pb-[40px]">
+                            <li className="flex gap-[8px] pb-[12px]">
+                                <p className="bg-orange1 text-white rounded-full w-[24px] h-[24px] text-center">2</p>
+                                <p className="text-[16px] text-orange1">Tuesday and Friday</p>
+                            </li>
+                            {
+                                data.map(el => {
+                                    if (el.days === "TF") {
+                                        return (
+                                            <li className="flex justify-between items-center pb-[10px]" onClick={() => handleClick(el.videoId)}>
+                                                <h2 className="text-white">{el.title}</h2>
+                                                <section className="flex gap-[22px]">
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={series} />
+                                                        <p className="text-white">{el.series}</p>
+                                                    </div>
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={repeats} />
+                                                        <p className="text-white">{el.repeats}</p>
+                                                    </div>
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={peso} />
+                                                        <p className="text-white">{el.peso}</p>
+                                                    </div>
+                                                </section>
+                                            </li>
+                                        )
+                                    }
+                                })
+                            }
+                        </ul>
+                    </section> :
+                    <section className="mb-[32px]">
+                        <ul className="pb-[20px]">
+                            <li className="pb-[16px] text-[18px] text-orange1">Today</li>
+                            {
+                                data.map(el => {
+                                    if (el.days === "MT") {
+                                        return (
+                                            <li className="flex justify-between items-center pb-[10px]" onClick={() => handleClick(el.videoId)}>
+                                                <h2 className="text-white">{el.title}</h2>
+                                                <section className="flex gap-[22px]">
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={series} />
+                                                        <p className="text-white">{el.series}</p>
+                                                    </div>
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={repeats} />
+                                                        <p className="text-white">{el.repeats}</p>
+                                                    </div>
+                                                    <div className="flex gap-[8px]">
+                                                        <img src={peso} />
+                                                        <p className="text-white">{el.peso}</p>
+                                                    </div>
+                                                </section>
+                                            </li>
+                                        )
+                                    }
+                                })
+                            }
+                        </ul>
+                        {
+                            isStart ?
+                                <div>
+                                    <p className="text-white text-[20px] text-center ">{execTitle[count]?.title}</p>
+                                    <p className="text-orange1 text-[20px] text-center mb-[16px]">Série {serie}</p>
+                                    <button style={{ margin: "0 auto" }}
+                                        onClick={handleExec}
+                                        className="bg-orange1 flex justify-center items-center w-[200px] h-[60px] text-white font-medium rounded-lg">Next</button>
+                                </div> :
+                                <button style={{ margin: "0 auto" }}
+                                    onClick={startExec}
+                                    className="bg-orange1 flex justify-center items-center w-[200px] h-[60px] text-white font-medium rounded-lg">Start</button>
                         }
-                    })
-                }
-            </ul>
-            <ul className="pb-[40px]">
-                <li className="flex gap-[8px] pb-[12px]">
-                    <p className="bg-orange1 text-white rounded-full w-[24px] h-[24px] text-center">2</p>
-                    <p className="text-[16px] text-orange1">Tuesday and Friday</p>
-                </li>
-                {
-                    data.map(el => {
-                        if (el.days === "TF") {
-                            return (
-                                <li className="flex justify-between items-center pb-[10px]" onClick={() => handleClick(el.videoId)}>
-                                    <h2 className="text-white">{el.title}</h2>
-                                    <section className="flex gap-[22px]">
-                                        <div className="flex gap-[8px]">
-                                            <img src={series} />
-                                            <p className="text-white">{el.series}</p>
-                                        </div>
-                                        <div className="flex gap-[8px]">
-                                            <img src={repeats} />
-                                            <p className="text-white">{el.repeats}</p>
-                                        </div>
-                                        <div className="flex gap-[8px]">
-                                            <img src={peso} />
-                                            <p className="text-white">{el.peso}</p>
-                                        </div>
-                                    </section>
-                                </li>
-                            )
-                        }
-                    })
-                }
-            </ul>
+
+                    </section>
+            }
             {
                 modal ?
                     <div>
